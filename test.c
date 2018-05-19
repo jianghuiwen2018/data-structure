d91081a76c8b3c33ae9b99c9dac6592a82f4901b
jianghuiwen2018 <1497911983@qq.com>
2018-05-19 10:06:32 +0800

    again

--- test.c
+++ test.c
@@ -6,13 +6,6 @@ this is operation in branch dev
 first commit in branch dev
 second commit in branch dev
 
-<<<<<<< HEAD
 first commit in branch master
-=======
 add merge
-<<<<<<< HEAD
->>>>>>> dev
-=======
-
 add no-ff
->>>>>>> dev
